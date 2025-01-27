055454611d5e9b05952957035a4f7f90271121e0
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-01-27 16:47:22 +0530

    Get actual data

--- src/Arnab.CustomPrompt/DataProviders/UserDataProvider.cs
+++ src/Arnab.CustomPrompt/DataProviders/UserDataProvider.cs
@@ -4,5 +4,5 @@
 public class UserDataProvider : DataProvider
 {
     /// <summary>Get data.</summary>
-    public override string GetData() => "Jon Doe";
+    public override string GetData() => Environment.UserName;
 }
