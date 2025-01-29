040b42c7fb5c8c53b681bebe08e9352e3b8d3f6d
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-01-29 09:28:24 +0530

    Xml comment in one line (#4)

--- src/Arnab.CustomPrompt/SectionProviders/GitSectionProvider.cs
+++ src/Arnab.CustomPrompt/SectionProviders/GitSectionProvider.cs
@@ -1,13 +1,9 @@
 ﻿namespace Arnab.CustomPrompt.SectionProviders;
 
-/// <summary>
-/// Git section provider.
-/// </summary>
+/// <summary>Git section provider.</summary>
 public class GitSectionProvider(IDataProvider dataProvider) : SectionProvider(dataProvider)
 {
-    /// <summary>
-    /// Get section.
-    /// </summary>
+    /// <summary>Get section.</summary>
     public override string GetSection()
     {
         var data = _dataProvider.GetData();
