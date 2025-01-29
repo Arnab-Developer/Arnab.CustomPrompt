b71ee2d8735e27b1b32df5817f1b16c43d585ea1
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-01-29 08:59:10 +0530

    api file

--- Example/Arnab.CustomPrompt.Api/Program.cs
+++ Example/Arnab.CustomPrompt.Api/Program.cs
@@ -1,31 +1,6 @@
-using Arnab.CustomPrompt.DataProviders;
-using Arnab.CustomPrompt.SectionProviders;
-using Arnab.CustomPrompt.Services;
+using Arnab.CustomPrompt.Api;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
-app.MapGet("/get-prompt", () =>
-{
-    var userDataProvider = new UserDataProvider();
-    var folderDataProvider = new FolderDataProvider();
-    var gitDataProvider = new GitDataProvider();
-
-    var userSectionProvider = new UserSectionProvider(userDataProvider);
-    var folderSectionProvider = new FolderSectionProvider(folderDataProvider);
-    var gitSectionProvider = new GitSectionProvider(gitDataProvider);
-
-    var sectionProviders = new List<ISectionProvider>
-    {
-        userSectionProvider,
-        folderSectionProvider,
-        gitSectionProvider
-    };
-
-    var promptService = new PromptService(sectionProviders);
-    var prompt = promptService.GetPrompt();
-
-    return Results.Ok(prompt);
-});
-
+app.MapPromptApi();
 app.Run();
